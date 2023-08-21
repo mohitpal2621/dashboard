25d70608559cd1e913c0530ed2e1d1963f9a5424
Mohit <mohitpal2621@gmail.com>
2023-08-22 00:15:51 +0530

    Routes

---
+++ dashboard/src/Components/Datalist.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function DataList() {
+    axios.defaults.baseURL = 'http://localhost:5000/api';   
+  const [dataList, setDataList] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('/data');
+        setDataList(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Data List</h2>
+      <ul>
+        {dataList.map(item => (
+          <li key={item._id}>
+            {item.title} - {item.country}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default DataList;
--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -15,4 +15,69 @@ router.get('/data', async (req, res) => {
   }
 });
 
+// Get data by sector
+router.get('/data/sector/:sector', async (req, res) => {
+  try {
+    const { sector } = req.params;
+    const dataBySector = await Data.find({ sector: sector });
+    res.json(dataBySector);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get data by region
+router.get('/data/region/:region', async (req, res) => {
+  try {
+    const { region } = req.params;
+    const dataByRegion = await Data.find({ region: region });
+    res.json(dataByRegion);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get data by source
+router.get('/data/source/:source', async (req, res) => {
+  try {
+    const { source } = req.params;
+    const dataBySource = await Data.find({ source: source });
+    res.json(dataBySource);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+// Get data by topic
+router.get('/data/topic/:topic', async (req, res) => {
+  try {
+    const { topic } = req.params;
+    const dataByTopic = await Data.find({ topic: sector });
+    res.json(dataByTopic);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get data by start year
+router.get('/data/start-year/:year', async (req, res) => {
+  try {
+    const { year } = req.params;
+    const filteredData = await Data.find({ start_year: year });
+    res.json(filteredData);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get data by end year
+router.get('/data/end-year/:year', async (req, res) => {
+  try {
+    const { year } = req.params;
+    const filteredData = await Data.find({ end_year: year });
+    res.json(filteredData);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 export default router;
